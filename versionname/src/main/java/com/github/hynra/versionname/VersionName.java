8b991e58be5830a9333387ca47a1284149303fa7
hynra <hynra@yahoo.com>
2018-05-16 15:36:49 +0700

    query with jsoup

--- versionname/src/main/java/com/github/hynra/versionname/VersionName.java
+++ versionname/src/main/java/com/github/hynra/versionname/VersionName.java
@@ -1,8 +1,37 @@
 package com.github.hynra.versionname;
 
+import android.os.AsyncTask;
+
+import org.jsoup.Jsoup;
+
 /**
  * Created by hynra on 16/05/2018.
  */
 
 public class VersionName {
+
+
+    private class GetVersionNmae extends AsyncTask<Void, String, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            String newVersion = "oops";
+            try {
+                newVersion = Jsoup.connect("https://play.google.com/store/apps/details?id=" + "app.pptik.itb.semut" + "&hl=en").timeout(30000)
+                        .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6").referrer("http://www.google.com")
+                        .get()
+                        .select(".hAyfc .htlgb")
+                        .get(5)
+                        .ownText();
+                return newVersion;
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String onlineVersion) {
+            super.onPostExecute(onlineVersion);
+
+        }
+    }
 }
